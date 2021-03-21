60bac4efc0b2dac696554c59d0e7688e8ce66f8f
Thomas Laforge <thomas.laforge.38@gmail.com>
2021-03-21 22:55:13 +0100

    spamming limit

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,34 +2,46 @@ require("dotenv").config();
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+const SPAM_LIMIT = 3 // nb email possible to send before spamming alert is up and block next messages
+
+let contacts = new Map<string, number>()
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const { name, email, text } = req.body;
+  const nbMailAlreadySent = contacts.get(email) || 0  
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    },
-  });
+  if(nbMailAlreadySent < SPAM_LIMIT){
+    const mailOption = {
+      from: `${email}`,
+      to: `${process.env.EMAIL}`,
+      subject: `New mail from ${email}`,
+      text: `
+      ${name} wrote:
+      ${text}
+      `,
+    };
 
-  const mailOption = {
-    from: `${email}`,
-    to: `${process.env.EMAIL}`,
-    subject: `New mail from ${email}`,
-    text: `
-    ${name} wrote:
-    ${text}
-    `,
-  };
-
-  transporter.sendMail(mailOption, (err: any, data: any) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("error" + JSON.stringify(err));
-    } else {
-      res.send("success");
-    }
-  });
+    transporter.sendMail(mailOption, (err: any, data: any) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("error" + JSON.stringify(err));
+      } else {
+        res.send("success");
+        contacts.set(email, nbMailAlreadySent + 1)
+      }
+    });
+  }
+  else {
+    contacts.set(email, nbMailAlreadySent + 1)
+    console.warn(`Spam Alert ! ${email} is spamming you (${contacts.get(email)} messages)`)
+    res.status(429).send("you are spamming me ...");
+  }
 };
