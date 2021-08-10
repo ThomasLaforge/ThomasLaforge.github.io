a4ec965eee12dc6d1975bb46f63aa2be2424a813
Thomas Laforge <thomas.laforge.38@gmail.com>
2021-08-10 15:10:12 +0200

    update blog rendering

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Head from "next/head";
 import matter from "gray-matter";
 import Link from "next/link";
+import Image from "next/image";
 import { GetStaticProps } from "next";
 import {ImCalendar} from 'react-icons/im'
+import fs from "fs";
 
+interface BlogIndexData {
+  content: string[],
+  miniatures: (string | null)[]
+}
 interface IndexProps {
-  data: any,
-  title: string,
-  description: string
+  data: BlogIndexData,
 }
 
 interface BlogData {
@@ -16,17 +20,12 @@ interface BlogData {
   title: string,
   description: string,
   date: string,
-  tags: string[]
-}
-
-interface BlogMatter {
-  data: BlogData
+  tags: string[],
 }
 
 const Index = ({ data }: IndexProps) => {
-  const RealData = data.map((blog: string) => matter(blog)) as BlogMatter[]
-  const ListItems = RealData.map((listItem) => listItem.data);
-
+  const { miniatures, content } = data
+  const blogDatas = content.map( blog => matter(blog).data as BlogData )
   return (
     <>
       <Head>
@@ -36,27 +35,43 @@ const Index = ({ data }: IndexProps) => {
         <h1>Bienvenue sur mon blog</h1>
         
         <div className='blog-collection'>
-          {ListItems.map((blog, i) => (
-            <div className='blog-summary' key={i}>
-              <Link href={`/blog/${blog.slug}`}>
-                <a className='blog-title'>{blog.title}</a>
-              </Link>
-              <div className="blog-meta">
-                <div className="blog-date">
-                  <ImCalendar className='date-icon'/> 
-                  <div className="date-value">{blog.date}</div>
-                </div>
-                {blog.tags && 
-                  <div className="blog-tags">
-                    {blog.tags.map((t, k) => (
-                      <div className="blog-tag-elt" key={k}>{t}</div>
-                    ))}
-                  </div>
+          {blogDatas.map((blog, i) => {
+            const miniature = miniatures[i]
+            return <div className='blog-summary' key={i}>
+              {/* <div className="blog-miniature">
+                {miniature 
+                  ? <Image 
+                      src={miniature} 
+                      className='miniature-image'
+                      width='150px'
+                      height='50px'
+                  />
+                  : 'empty'
                 }
+              </div> */}
+
+              <div className="blog-text-data">
+
+                <Link href={`/blog/${blog.slug}`}>
+                  <a className='blog-title'>{blog.title}</a>
+                </Link>
+                <div className="blog-meta">
+                  <div className="blog-date">
+                    <ImCalendar className='date-icon'/> 
+                    <div className="date-value">{blog.date}</div>
+                  </div>
+                  {blog.tags && 
+                    <div className="blog-tags">
+                      {blog.tags.map((t, k) => (
+                        <div className="blog-tag-elt" key={k}>{t}</div>
+                      ))}
+                    </div>
+                  }
+                </div>
+                <div className='blog-description'>{blog.description}</div>
               </div>
-              <div className='blog-description'>{blog.description}</div>
             </div>
-          ))}
+          })}
         </div>
       </div>
     </>
@@ -66,27 +81,23 @@ const Index = ({ data }: IndexProps) => {
 export default Index;
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const siteData = await import(`../../config.json`);
-  const fs = require("fs");
-
   const files = fs.readdirSync(`${process.cwd()}/content`, "utf-8");
+  const blogSlugs: string[] = files.filter((fn: string) => fn.endsWith(".md"));
 
-  const blogs = files.filter((fn: string) => fn.endsWith(".md"));
-
-  const data = blogs.map((blog: string) => {
-    const path = `${process.cwd()}/content/${blog}`;
-    const rawContent = fs.readFileSync(path, {
-      encoding: "utf-8",
-    });
-
-    return rawContent;
+  const data = blogSlugs.map((blogSlug) => {
+    const path = `${process.cwd()}/content/${blogSlug}`;
+    const rawContent: string = fs.readFileSync(path, { encoding: "utf-8" });
+    return rawContent
   });
 
   return {
-    props: {
-      data: data,
-      title: siteData.default.title,
-      description: siteData.default.description,
-    },
+    props: { data : {
+      content: data,
+      miniatures: blogSlugs.map( blogFileName => {
+        const slug = blogFileName.replace('.md', '')
+        const path = `${process.cwd()}/public/blog_images/${slug}/_miniature.png`;
+        return fs.existsSync(path) ? `/blog_images/${slug}/_miniature.png` : null 
+      })
+    } } as IndexProps,
   };
 }
